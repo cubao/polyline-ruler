2a5c5105641b60e87c01e9e9f6f60dde9ae6b096
TANG ZhiXiong <district10@users.noreply.github.com>
2023-06-23 01:31:59 +0800

    for wgs84 coords in PolylineRuler, convert lla2enu only once (#18)
    * update headers
    
    * update build script
    
    * update linux image
    
    * misc updates
    
    * add cheapRuler
    
    * init cheap ruler
    
    * more cheap ruler bindings, tests
    
    * better performance for wgs84 coords
    
    * more binding, should be faster
    
    * update docs
    
    * lint code
    
    * better
    
    * fix
    
    * update headers (synced)
    
    ---------
    
    Co-authored-by: TANG ZHIXIONG <zhixiong.tang@momenta.ai>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,6 +2,7 @@ import time
 
 import numpy as np
 from polyline_ruler import (
+    CheapRuler,
     LineSegment,
     PolylineRuler,
     douglas_simplify,
@@ -392,3 +393,45 @@ def test_douglas():
         epsilon=1e-9,
     )
     assert np.all(indexes == [0, 3])
+
+
+def test_cheap_ruler():
+    assert CheapRuler.RE
+    assert CheapRuler.FE
+    assert CheapRuler.E2
+    assert CheapRuler.RAD
+    print(CheapRuler.Unit.__members__)
+    assert CheapRuler.Unit.Meters == CheapRuler.Unit.Metres
+    print(CheapRuler.Unit.Kilometers)
+    assert str(CheapRuler.Unit.Kilometers) == "Unit.Kilometers"
+    assert int(CheapRuler.Unit.Kilometers) == 0
+    k1 = CheapRuler._k(20.0)
+    k2 = CheapRuler._k(20.0, unit=CheapRuler.Unit.Kilometers)
+    assert np.linalg.norm(k1 - k2 * 1000) < 1e-8
+
+    ruler = CheapRuler(45.0)
+    np.testing.assert_allclose(ruler.k(), [78846.8350939781, 111131.7774141756, 1.0])
+
+    CheapRuler._fromTile(10, 32)
+    ruler.squareDistance([120, 34, 5], [121, 34, 5])
+    assert (
+        ruler.distance([120, 34, 5], [121, 34, 5])
+        == ruler.delta([120, 34, 5], [121, 34, 5])[0]
+    )
+
+    assert ruler.bearing([120, 34, 5], [121, 34, 5]) == 90.0
+    assert ruler.bearing([122, 34, 5], [121, 34, 5]) == -90.0
+    assert ruler.bearing([120, 34, 5], [120, 33, 5]) == 180.0
+
+    ruler.destination([120, 34, 5], 100, 0)
+    ruler.destination([120, 34, 5], 100, 90)
+
+    pt = ruler.offset([120, 34, 5], 300, 400)
+    assert abs(ruler.distance([120, 34, 5], pt) - 500) < 1e-6
+    dist = ruler.lineDistance(
+        [
+            [120, 34, 5],
+            [120, 35, 5],
+        ]
+    )
+    assert dist == ruler.k()[1]
