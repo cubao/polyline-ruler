79a710ad4aa06f7325130a75e758040d65f94c47
tang zhixiong <zhixiong.tang@momenta.ai>
2024-10-03 00:46:03 +0800

    fix

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import time
 
 import numpy as np
@@ -16,9 +18,9 @@ from polyline_ruler.tf import cheap_ruler_k
 
 def test_segment():
     seg = LineSegment([0, 0, 0], [10, 0, 0])
-    assert 4.0 == seg.distance([5.0, 4.0, 0.0])
-    assert 5.0 == seg.distance([-4.0, 3.0, 0.0])
-    assert 5.0 == seg.distance([14.0, 3.0, 0.0])
+    assert seg.distance([5.0, 4.0, 0.0]) == 4.0
+    assert seg.distance([-4.0, 3.0, 0.0]) == 5.0
+    assert seg.distance([14.0, 3.0, 0.0]) == 5.0
 
     assert np.all(seg.A == [0, 0, 0])
     assert np.all(seg.B == [10, 0, 0])
@@ -27,9 +29,9 @@ def test_segment():
     assert seg.length2 == 100.0
 
     seg = LineSegment([0, 0, 0], [0, 0, 0])
-    assert 5.0 == seg.distance([3.0, 4.0, 0.0])
-    assert 5.0 == seg.distance([-4.0, 3.0, 0.0])
-    assert 13.0 == seg.distance([5.0, 12.0, 0.0])
+    assert seg.distance([3.0, 4.0, 0.0]) == 5.0
+    assert seg.distance([-4.0, 3.0, 0.0]) == 5.0
+    assert seg.distance([5.0, 12.0, 0.0]) == 13.0
 
 
 def test_transform():
